4cc27a9962de17704982c78d3bdcc5334205345c
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-06-18 22:47:35 +0900

    make exception messages simple

--- py39/log.py
+++ py39/log.py
@@ -1,25 +1,40 @@
-from logging import getLogger, Formatter, StreamHandler, FileHandler, DEBUG
+from logging import getLogger, Formatter, StreamHandler, FileHandler, DEBUG, WARN
+# import traceback
 
-logger = getLogger(__name__)
-fmt = Formatter('%(asctime)s %(filename)s:%(funcName)s:%(lineno)d [%(levelname)s]%(message)s')
+class TestLogger():
+    def __init__(self):
+        self.logger = getLogger(__name__)
+        fmt = Formatter('%(asctime)s %(filename)s:%(funcName)s:%(lineno)d [%(levelname)s]%(message)s')
 
-sh = StreamHandler()
-sh.setLevel(DEBUG)
-sh.setFormatter(fmt)
+        sh = StreamHandler()
+        sh.setLevel(WARN)
+        sh.setFormatter(fmt)
 
-fh = FileHandler('test.log', 'a+')
-fh.setLevel(DEBUG)
-fh.setFormatter(fmt)
+        fh = FileHandler('test.log', 'a+')
+        fh.setLevel(DEBUG)
+        fh.setFormatter(fmt)
 
-logger.setLevel(DEBUG)
-logger.addHandler(sh)
-logger.addHandler(fh)
-logger.propagate = False
+        self.logger.setLevel(DEBUG)
+        self.logger.addHandler(sh)
+        self.logger.addHandler(fh)
+        self.logger.propagate = False
 
-try:
-    logger.info('Info')
-    logger.warning('Warn')
-    logger.error('Error')
-    raise ValueError('ValueError')
-except Exception as e:
-    logger.exception(e)
+def main():
+    try:
+        logger = TestLogger().logger
+        logger.debug('Debug')
+        logger.info('Info')
+        logger.warning('Warn')
+        logger.error('Error')
+        raise ValueError('Error test')
+        # raise ZeroDivisionError('Error test')
+    except Exception as e:
+        ### Error class and message only
+        logger.error('{}: {}'.format(str(e.__class__.__name__), str(e)))
+        # logger.error(traceback.format_exception_only(type(e), e)[0].rstrip('\n'))
+
+        ### With stacktrace
+        # logger.exception(e)
+
+if __name__ == '__main__':
+    main()
