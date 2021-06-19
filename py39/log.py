db5df30d4c3c0f1ec4a300e9308c4ac24fed80aa
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-06-19 18:59:31 +0900

    add config files

--- py39/log.py
+++ py39/log.py
@@ -1,40 +1,68 @@
-from logging import getLogger, Formatter, StreamHandler, FileHandler, DEBUG, WARN
-# import traceback
+from time import gmtime
+from logging import config, getLogger, Formatter, StreamHandler, FileHandler, DEBUG, WARN
+from traceback import format_exception_only
 
-class TestLogger():
+from yaml import safe_load
+
+CONFIG_FILE = 'logging.yaml'
+
+class ConfiguredLogger():
     def __init__(self):
-        self.logger = getLogger(__name__)
-        fmt = Formatter('%(asctime)s %(filename)s:%(funcName)s:%(lineno)d [%(levelname)s]%(message)s')
+        with open(CONFIG_FILE) as file:
+            conf = safe_load(file)
+        config.dictConfig(conf)
+        Formatter.converter = gmtime
+    
+    def create(self, name):
+        return getLogger(name)
 
-        sh = StreamHandler()
-        sh.setLevel(WARN)
-        sh.setFormatter(fmt)
+class HandmadeLogger():
+    def __init__(self):
+        default_fmt = Formatter()
+        default_fmt.converter = gmtime
+        FMT = '%(asctime)s.%(msecs)03d %(filename)s:%(funcName)s:%(lineno)d [%(levelname)s]%(message)s'
+        DATE_FMT = '%Y-%m-%d %H:%M:%S'
+        verbose_fmt = Formatter(fmt=FMT, datefmt=DATE_FMT, style='%')
+        verbose_fmt.converter = gmtime
 
-        fh = FileHandler('test.log', 'a+')
-        fh.setLevel(DEBUG)
-        fh.setFormatter(fmt)
+        self.__sh = StreamHandler()
+        self.__sh.setLevel(WARN)
+        self.__sh.setFormatter(default_fmt)
 
-        self.logger.setLevel(DEBUG)
-        self.logger.addHandler(sh)
-        self.logger.addHandler(fh)
-        self.logger.propagate = False
+        self.__fh = FileHandler('test.log', 'a+')
+        self.__fh.setLevel(DEBUG)
+        self.__fh.setFormatter(verbose_fmt)
+
+    def create(self, name):
+        logger = getLogger(name)
+        logger.setLevel(DEBUG)
+        logger.addHandler(self.__sh)
+        logger.addHandler(self.__fh)
+        logger.propagate = False
 
 def main():
     try:
-        logger = TestLogger().logger
+        logger = ConfiguredLogger().create(__name__)
         logger.debug('Debug')
         logger.info('Info')
         logger.warning('Warn')
         logger.error('Error')
+        logger.critical('Critical')
         raise ValueError('Error test')
-        # raise ZeroDivisionError('Error test')
+
     except Exception as e:
-        ### Error class and message only
-        logger.error('{}: {}'.format(str(e.__class__.__name__), str(e)))
-        # logger.error(traceback.format_exception_only(type(e), e)[0].rstrip('\n'))
+        ### Error message only
+        logger.error(e)
+        logger.exception(e, exc_info=False)
 
         ### With stacktrace
-        # logger.exception(e)
+        logger.error(e, exc_info=True)
+        logger.exception(e)
+
+        ### Without stacktrace
+        logger.error('{}: {}'.format(str(e.__class__.__name__), str(e)))
+        logger.error(format_exception_only(type(e), e)[0].rstrip('\n'))
+
 
 if __name__ == '__main__':
     main()
