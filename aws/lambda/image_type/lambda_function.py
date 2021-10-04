f3490019732ef4ed56cccf4848cdf3fb9f9fb03d
tosh223 <rev.to12@gmail.com>
2021-10-04 18:25:50 +0900

    update lambda image

--- aws/lambda/image_type/lambda_function.py
+++ aws/lambda/image_type/lambda_function.py
@@ -4,17 +4,16 @@ import logging
 import time
 import traceback
 
-def set_logger():
-    level = logging.getLevelName(os.environ.get('LOG_LEVEL'))
-    FMT = '%(asctime)s.%(msecs)03d\t%(aws_request_id)s\t%(filename)s:%(funcName)s:%(lineno)d\t[%(levelname)s]%(message)s'
-    DATE_FMT = '%Y-%m-%d %H:%M:%S'
-    fmt = logging.Formatter(fmt=FMT, datefmt=DATE_FMT, style='%')
-    fmt.converter = time.gmtime
+level = logging.getLevelName(os.environ.get('LOG_LEVEL'))
+FMT = '%(asctime)s.%(msecs)03d\t%(aws_request_id)s\t%(filename)s:%(funcName)s:%(lineno)d\t[%(levelname)s]%(message)s\n'
+DATE_FMT = '%Y-%m-%d %H:%M:%S'
+fmt = logging.Formatter(fmt=FMT, datefmt=DATE_FMT, style='%')
+fmt.converter = time.gmtime
 
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
-    root_logger.handlers[0].setLevel(level)
-    root_logger.handlers[0].setFormatter(fmt)
+root_logger = logging.getLogger()
+root_logger.setLevel(level)
+root_logger.handlers[0].setLevel(level)
+root_logger.handlers[0].setFormatter(fmt)
 
 def main(event, context):
     logger = logging.getLogger(__name__)
@@ -38,5 +37,4 @@ def main(event, context):
         logger.error(err_msg)
 
 def lambda_handler(event, context):
-    set_logger()
     main(event, context)
